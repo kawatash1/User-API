893c3f260f10c486a0abb0db8345127f784894bb
Katya <92756386+kawatash1@users.noreply.github.com>
2024-12-11 16:08:54 +0200

    Update authMiddleware.js

--- utils/authMiddleware.js
+++ utils/authMiddleware.js
@@ -4,12 +4,12 @@ const authenticateToken = (req, res, next) => {
   const token = req.headers['authorization']?.split(' ')[1]; // Извлекаем токен из заголовка
 
   if (!token) {
-    return res.status(401).json({ message: 'Токен не предоставлен' });
+    return res.status(401).json({ message: 'No token' });
   }
 
   jwt.verify(token, process.env.JWT_SECRET, (err, user) => {
     if (err) {
-      return res.status(403).json({ message: 'Неверный или просроченный токен' });
+      return res.status(403).json({ message: 'Invalid or expired token' });
     }
     req.user = user; // Делаем данные пользователя доступными для последующих маршрутов
     next();
